b9052a9c3180e8c208430e6864c95c25c6eeb831
TOM DUFFY <tduffy@toms-mbp.attlocal.net>
2016-04-14 23:22:18 -0700

    Moving from procedural to objects

---
+++ classes/WaterData.php
@@ -0,0 +1,52 @@
+<?php
+
+class WaterData{
+
+	//function to get water data for given site
+	public function getWaterData($site)
+	{
+		if($site == 'time'){
+          $string = file_get_contents('http://waterservices.usgs.gov/nwis/iv/?sites=10348000&format=json');
+		  $json = json_decode($string, true);
+		  $siteName = ($json["value"]["timeSeries"][0]["sourceInfo"]["siteName"]);
+		  $currentFlowRate = ($json["value"]["timeSeries"][0]["values"][0]["value"][0]["value"]);
+		
+		  if($currentFlowRate <= 0)
+		  {
+			$currentFlowRate = "Data Error";
+		  }
+
+		  $machineDateTime = ($json["value"]["timeSeries"][0]["values"][0]["value"][0]["dateTime"]);
+		  $dt = DateTime::createFromFormat('Y-m-d\TH:i:s+', $machineDateTime);
+		  $humanDate = $dt->format("M d, Y");
+		  $humanTime = $dt->format("g:i A");
+		  $time = array($humanDate, $humanTime);
+		  return $time;
+		}
+		
+		$string = file_get_contents($site);
+		$json = json_decode($string, true);
+		$siteName = ($json["value"]["timeSeries"][0]["sourceInfo"]["siteName"]);
+		$currentFlowRate = ($json["value"]["timeSeries"][0]["values"][0]["value"][0]["value"]);
+		
+		if($currentFlowRate <= 0)
+		{
+			$currentFlowRate = "Data Error";
+		}
+
+		$machineDateTime = ($json["value"]["timeSeries"][0]["values"][0]["value"][0]["dateTime"]);
+		$dt = DateTime::createFromFormat('Y-m-d\TH:i:s+', $machineDateTime);
+		$humanDate = $dt->format("M d, Y");
+		$humanTime = $dt->format("g:i A");
+
+		if($currentFlowRate != "Data Error")
+		{
+			$printOut = '<span class="flowrate">' . $currentFlowRate . '</span> <span class="units">cfs</span>';
+		}
+		else
+		{
+			$printOut = '<span class="flowrate red">' . $currentFlowRate . '</span>';
+		}
+		return $printOut;
+	}
+}
